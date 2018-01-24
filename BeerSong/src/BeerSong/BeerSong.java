e37f3413ac87841580d5d0add382e6dab881bf7f
Faboover <fjlefevre@yahoo.com>
2018-01-24 16:53:40 -0500

    BeerSong.java with actual empty functions Main and 99 beers(not actual method call)

--- BeerSong/src/BeerSong/BeerSong.java
+++ BeerSong/src/BeerSong/BeerSong.java
@@ -1,6 +1,15 @@
 
 package BeerSong;
 
-public class BeerSong {
+public class BeerSong
+{
+    public static void Ninety_Nine_Bottles_of_Beer()
+    {
+        
+    }
     
+    public static void main(String[] args) 
+    {
+        
+    }
 }
